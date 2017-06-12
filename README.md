780c6b672a9708032c9e3b1def23b032383b3670
vlaridus <vlaridus@gmail.com>
2017-06-12 17:26:07 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pokemon Showdown Account Checker
+### Check if a list of accounts are taken
+
+## Usage
+- `python account_checker.py -i <input_file.txt>`
+
+## Dependencies
+- Python 3
+- argparse
+- requests
+- colorama
+- time
+## Examples
+- Sample word list: https://github.com/dwyl/english-words/blob/master/words.txt?raw=true
---
+++ account_checker.py
@@ -0,0 +1,38 @@
+import colorama
+import argparse
+import requests
+import time
+
+def check_username(usernames):
+    unregistered = 0
+    time_string = time.strftime("%H_%M_%S")
+    output = open(time_string + '.txt', 'a+')
+    output.truncate()
+    output.write('UNREGISTERED NAMES:\n')
+    for user in usernames:
+        url = 'https://pokemonshowdown.com/users/{}.json'.format(user)
+        data = requests.get(url)
+        parsed_data = data.json()
+        if parsed_data['registertime'] == 0:
+            print(colorama.Fore.GREEN + user)
+            unregistered += 1
+            temp = '{}\n'.format(user)
+            output.write(temp)
+
+    output.close()
+    count = len(usernames)
+    print(colorama.Fore.YELLOW + 'Done. {}/{} unregistered, {}%'.format(count,count, unregistered / count))
+
+
+def start():
+    colorama.init()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input", help="Text file as input")
+    arg = parser.parse_args()
+
+    with open(arg.input) as input_file:
+        usernames = input_file.read().splitlines()
+        check_username(usernames)
+
+if __name__ == '__main__':
+    start()
